3a237d5c5e1c1699ae18d6faa190d710ef790849
Vlad Niculescu <vlad.nic@gmail.com>
2023-05-28 17:47:38 +0100

    small

--- Libs/PowMaybe/Extensions/MayDictionaryExt.cs
+++ Libs/PowMaybe/Extensions/MayDictionaryExt.cs
@@ -3,7 +3,7 @@ namespace PowMaybe;
 
 public static class MayDictionaryExt
 {
-	public static Maybe<V> GetOrMaybe<K, V>(this Dictionary<K, V> dict, K key) where K : notnull => dict.TryGetValue(key, out var val) switch
+	public static Maybe<V> GetOrMaybe<K, V>(this IReadOnlyDictionary<K, V> dict, K key) where K : notnull => dict.TryGetValue(key, out var val) switch
 	{
 		true => May.Some(val!),
 		false => May.None<V>()
