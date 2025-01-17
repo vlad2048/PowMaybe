fdcec1d78127116ad6e1fb0bb1c0f902e570b752
Vlad Niculescu-Bolintin <vlad.nic@gmail.com>
2025-01-17 22:36:06 +0000

    .net 9

--- package.bat
+++ package.bat
@@ -1,5 +1,5 @@
 rem nuget setapikey xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -Source https://api.nuget.org/v3/index.json
-set ver=0.1.0
+set ver=0.2.0
 set slnFolder=D:\dev\nuget\PowMaybe
 set nugetFolder=C:\Users\admin\.nuget\packages
 set nugetUrl=https://api.nuget.org/v3/index.json
